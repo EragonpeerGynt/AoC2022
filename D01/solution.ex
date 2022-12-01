72be63463b9a3a1af4d448603f7dd42452efd6cf
EragonpeerGynt <EragonpeerGynt@users.noreply.github.com>
2022-12-01 09:55:59 +0000

    Removed everyting from previous year, so I have clean image

--- D01/solution.ex
+++ D01/solution.ex
@@ -1,38 +1,9 @@
 defmodule D01 do
-	def part1(_, [], total) do
-		total
-	end
-	def part1(previous, [head|tail], total) do
-		case (head - previous) do
-			n when n > 0 -> D01.part1(head, tail, total+1)
-			_ -> D01.part1(head, tail, total)
-		end
-	end
-	def part2(_, [_], total) do
-		total
-	end
-	def part2(previoussum, [head|tail], total) do
-		currentsum = tail |> Enum.take(2) |> Enum.sum()
-		currentsum = currentsum + head
-		case (currentsum - previoussum) do
-			n when n > 0 -> D01.part2(currentsum, tail, total+1)
-			_ -> D01.part2(currentsum, tail, total)
-		end
-	end
+	
 end
 
 defmodule Main do
 	def execute(day) do
 		input = Input.file(day) 
-			|> Input.split("\n")
-			|> Enum.map(fn x -> Integer.parse(x) end) 
-			|> Enum.map(fn {x,_} -> x end)
-
-		[head|tail] = input
-
-		D01.part1(head, tail, 0) |> IO.puts
-
-		firstsum = input |> Enum.take(3) |> Enum.sum()
-		D01.part2(firstsum, tail, 0) |> IO.puts
 	end
 end
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-DAY="D16"
+DAY="D01"
 rm -f *.beam
 elixirc Common/FileReader.ex
 if [ -f ${DAY}/solution.ex ]; then
